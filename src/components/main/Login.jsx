ce6192521e950b89159d3a594026ea59b7310e15
Robbi Wafik <robbiwafik31@gmail.com>
2022-08-31 15:27:28 +0700

    ref: make the Login component inherited from Form component

--- src/components/main/Login.jsx
+++ src/components/main/Login.jsx
@@ -1,8 +1,44 @@
-import React, { Component } from 'react';
+import Joi from 'joi';
+import React from 'react';
 import { FaLock, FaUser } from 'react-icons/fa';
+import Form from '../utils/Form';
 
-class Login extends Component {
+class Login extends Form {
+    state = {
+        data: {
+            username: '',
+            password: ''
+        },
+        errors: {}
+    };
+
+    schema = {
+        username: 
+            Joi
+            .string()
+            .alphanum()
+            .required()
+            .messages({
+                'string.empty': 'Kolom username tidak boleh kosong.',
+                'string.alphanum': 'Username hanya bisa menggunakan angka dan huruf.',
+            }),
+        password: 
+            Joi
+            .string()
+            .alphanum()
+            .required()
+            .messages({
+                'string.empty': 'Kolom password tidak boleh kosong.',
+                'string.alphanum': 'Password hanya bisa menggunakan angka dan huruf.'
+            })
+    };
+
+    doSubmit() {
+        console.log('Submitted');
+    }
+
     render() {
+
         return (
             <div className="login-wrapper">
                 <div className="card">
@@ -12,16 +48,10 @@ class Login extends Component {
                         </div>
                         <div className='column login__form-wrapper'>
                             <span className='login__title'>Admin Login</span>
-                            <form action="" className='login__form'>
-                                <div className='input-wrapper'>
-                                    <input className='input' type="text" placeholder='Username' />
-                                    <FaUser className='input__icon'  />
-                                </div>
-                                <div className='input-wrapper'>
-                                    <input className='input' type="text" placeholder='Password' />
-                                    <FaLock className='input__icon' />
-                                </div>
-                                <input className='btn btn--primary login__btn' type="button" value='Login' />
+                            <form action="" className='login__form' onSubmit={this.handleSubmit}>
+                                {this.renderInput(FaUser, 'username', {placeholder: 'Username', autoComplete: 'off', autoFocus: true})}
+                                {this.renderInput(FaLock, 'password', {placeholder: 'Password', type: 'password'})}
+                                <button className='btn btn--primary login__btn'>LOGIN</button>
                             </form>
                         </div>
                     </div>
